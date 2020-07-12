2ac4e313d336964ef4a9707c097da5f383ec7fcc
Alex <hungryhex@gmail.com>
2020-07-13 02:50:26 +1000

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const startGameBtn = document.getElementById("start-game-btn");
+
+function startGame() {
+  console.log("The game is starting...");
+}
+
+startGameBtn.addEventListener("click", startGame);
