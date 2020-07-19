f29f492b80dda203b609c7ecd2eda5beafa37720
Alex <hungryhex@gmail.com>
2020-07-19 23:19:47 +1000

    Added function to get user's choice

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 const startGameBtn = document.getElementById("start-game-btn");
 
-function startGame() {
-  console.log("The game is starting...");
-}
+const SELECTION_ROCK = "ROCK";
+const SELECTION_PAPER = "PAPER";
+const SELECTION_SCISSORS = "SCISSORS";
+const DEFAULT_USER_CHOICE = SELECTION_ROCK;
 
-startGameBtn.addEventListener("click", startGame);
+const getPlayerChoice = function () {
+  const selection = prompt(
+    `${SELECTION_ROCK}, ${SELECTION_PAPER} OR ${SELECTION_SCISSORS}?`,
+    ""
+  ).toUpperCase();
+
+  if (
+    selection !== SELECTION_ROCK &&
+    selection !== SELECTION_PAPER &&
+    selection !== SELECTION_SCISSORS
+  ) {
+    alert(`Invalid input! We chose ${DEFAULT_USER_CHOICE} for you!`);
+    return DEFAULT_USER_CHOICE;
+  }
+  return selection;
+};
+
+startGameBtn.addEventListener("click", function () {
+  console.log("The game is starting...");
+  const playerSelection = getPlayerChoice();
+  console.log(playerSelection);
+});
